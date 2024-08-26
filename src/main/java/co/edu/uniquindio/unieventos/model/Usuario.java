bde28eb4b3d713d1650df823a29976d8aefe49df
JulianNNN9 <111833091+JulianNNN9@users.noreply.github.com>
2024-08-26 12:08:54 -0500

    Se agrega la imagen del diagrama uml terminado.

--- src/main/java/co/edu/uniquindio/unieventos/model/Usuario.java
+++ src/main/java/co/edu/uniquindio/unieventos/model/Usuario.java
@@ -3,7 +3,6 @@ package co.edu.uniquindio.unieventos.model;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import java.util.List;
 
 @Document("usuarios")
 @NoArgsConstructor
