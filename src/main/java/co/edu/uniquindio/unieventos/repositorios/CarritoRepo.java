829062acce7d1c08a7f06892e8d66b065ee2b072
JulianNNN9 <111833091+JulianNNN9@users.noreply.github.com>
2024-08-26 10:14:22 -0500

    Agrego algunos repos, y borro una clase de service inutil.

--- src/main/java/co/edu/uniquindio/unieventos/repositorios/CarritoRepo.java
+++ src/main/java/co/edu/uniquindio/unieventos/repositorios/CarritoRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompraRepo extends MongoRepository<Compra, String> {
+public interface CarritoRepo extends MongoRepository<Compra, String> {
 }
