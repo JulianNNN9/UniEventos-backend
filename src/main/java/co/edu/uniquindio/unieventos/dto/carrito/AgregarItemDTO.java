27d99aae65b81772238a539ccebbea3d05329fc1
JulianNNN9 <111833091+JulianNNN9@users.noreply.github.com>
2024-09-02 19:14:39 -0500

    Alguna implementacion, y cambios en los servicios.

---
+++ src/main/java/co/edu/uniquindio/unieventos/dto/carrito/AgregarItemDTO.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.unieventos.dto.carrito;
+
+import co.edu.uniquindio.unieventos.model.DetalleCarrito;
+
+public record AgregarItemDTO (
+        String idCarrito,
+        DetalleCarrito detalleCarrito
+){
+}
--- src/main/java/co/edu/uniquindio/unieventos/dto/cuenta/CrearCuentaDTO.java
+++ src/main/java/co/edu/uniquindio/unieventos/dto/cuenta/CrearCuentaDTO.java
@@ -1,13 +1,17 @@
 package co.edu.uniquindio.unieventos.dto.cuenta;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
 public record CrearCuentaDTO(
 
-    String cedula,
-    String nombreCompleto,
-    String direccion,
-    String telefono,
-    String email,
-    String contrasenia
+    @NotBlank @Length(max = 10) String cedula,
+    @NotBlank @Length(max = 25) String nombreCompleto,
+    @Length(max = 10) String direccion,
+    @Length(max = 10) String telefono,
+    @NotBlank @Length(max = 50) @Email String email,
+    @NotBlank @Length(min = 6 ) @Length(max = 20) String contrasenia
 
 ){
 }
