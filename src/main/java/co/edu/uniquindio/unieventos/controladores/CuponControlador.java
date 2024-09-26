35c9f414b908f246573f010a70c13717cb5457c2
Nicolás Jurado <99767358+NicholashiG@users.noreply.github.com>
2024-09-26 16:34:19 -0500

    Se crean los RestControllers
    Idealmente, chequear que los controladores funcionen
    
    Queda pendiente implementar dos métodos:
    
    CompraControlador: recibirNotificacionMercadoPago (Línea 49)
    
    EventoControlador:
    filtrarEvento (Línea 44)
    
    Btw, al parecer, hay un error el FirebaseConfig.java al momento de ejecutar la aplicación

---
+++ src/main/java/co/edu/uniquindio/unieventos/controladores/CuponControlador.java
@@ -0,0 +1,39 @@
+package co.edu.uniquindio.unieventos.controladores;
+
+import co.edu.uniquindio.unieventos.dto.MensajeDTO;
+import co.edu.uniquindio.unieventos.dto.cupon.CrearEditarCuponDTO;
+import co.edu.uniquindio.unieventos.model.Cupon;
+import co.edu.uniquindio.unieventos.services.interfaces.CuponService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cupones")
+@RequiredArgsConstructor
+public class CuponControlador {
+
+    private final CuponService cuponService;
+
+    @PostMapping ("/crear-cupon")
+    public ResponseEntity<MensajeDTO<String>> crearCupon(@Valid @RequestBody CrearEditarCuponDTO crearEditarCuponDTO) throws Exception {
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, cuponService.crearCupon(crearEditarCuponDTO)));
+    }
+
+    @PostMapping ("/editar-cupon")
+    public ResponseEntity<MensajeDTO<String>> editarCupon(@Valid @RequestBody CrearEditarCuponDTO crearEditarCuponDTO) throws Exception {
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, cuponService.editarCupon(crearEditarCuponDTO)));
+    }
+
+    @GetMapping ("/eliminar-cupon/{idCupon}")
+    public ResponseEntity<MensajeDTO<String>> eliminarCupon(@PathVariable String idCupon) throws Exception {
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, cuponService.eliminarCupon(idCupon)));
+    }
+
+    @GetMapping ("/obtener-cupon/{idCupon}")
+    public ResponseEntity<MensajeDTO<Cupon>> obtenerCupon(@PathVariable String idCupon) throws Exception {
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, cuponService.obtenerCupon(idCupon)));
+    }
+
+}
---
+++ src/main/java/co/edu/uniquindio/unieventos/controladores/EventoControlador.java
@@ -0,0 +1,66 @@
+package co.edu.uniquindio.unieventos.controladores;
+
+import co.edu.uniquindio.unieventos.dto.MensajeDTO;
+import co.edu.uniquindio.unieventos.dto.evento.CrearEventoDTO;
+import co.edu.uniquindio.unieventos.dto.evento.EditarEventoDTO;
+import co.edu.uniquindio.unieventos.dto.evento.InformacionEventoDTO;
+import co.edu.uniquindio.unieventos.dto.evento.ItemEventoDTO;
+import co.edu.uniquindio.unieventos.model.Evento;
+import co.edu.uniquindio.unieventos.services.interfaces.EventoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/eventos")
+@RequiredArgsConstructor
+public class EventoControlador {
+
+    private final EventoService eventoService;
+
+    @PostMapping ("/crear-evento")
+    public ResponseEntity<MensajeDTO<String>> crearEvento(@Valid @RequestBody CrearEventoDTO crearEventoDTO) throws Exception {
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, eventoService.crearEvento(crearEventoDTO)));
+    }
+
+    @PostMapping ("/editar-evento")
+    public ResponseEntity<MensajeDTO<String>> editarEvento(@Valid @RequestBody EditarEventoDTO editarEventoDTO) throws Exception {
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, eventoService.editarEvento(editarEventoDTO)));
+    }
+
+    @GetMapping ("/eliminar-evento/{idEvento}")
+    public ResponseEntity<MensajeDTO<String>> eliminarEvento(@PathVariable String idEvento) throws Exception {
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, eventoService.eliminarEvento(idEvento)));
+    }
+
+    @GetMapping ("/obtener-informacion-evento/{idEvento}")
+    public ResponseEntity<MensajeDTO<InformacionEventoDTO>> obtenerInformacionEvento(@PathVariable String idEvento) throws Exception {
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, eventoService.obtenerInformacionEvento(idEvento)));
+    }
+
+    // todo implementar lo de filtrar eventos
+
+    @GetMapping ("/buscar-evento/{valorCampoDeBusqueda}")
+    public ResponseEntity<MensajeDTO<List<ItemEventoDTO>>> buscarEvento(@PathVariable String valorCampoDeBusqueda) throws Exception {
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, eventoService.buscarEvento(valorCampoDeBusqueda)));
+    }
+
+    @GetMapping ("/listar-eventos")
+    public ResponseEntity<MensajeDTO<List<ItemEventoDTO>>> listarEventos() throws Exception {
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, eventoService.listarEventos()));
+    }
+
+    @GetMapping ("/obtener-evento/{idEvento}")
+    public ResponseEntity<MensajeDTO<Evento>> obtenerEvento(@PathVariable String idEvento) throws Exception {
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, eventoService.obtenerEvento(idEvento)));
+    }
+
+    @PostMapping ("/save-evento")
+    public ResponseEntity<MensajeDTO<String>> saveEvento(@Valid @RequestBody Evento evento) throws Exception {
+        eventoService.saveEvento(evento);
+        return ResponseEntity.ok().body(new MensajeDTO<>(false, "Evento guardado correctamente"));
+    }
+}
